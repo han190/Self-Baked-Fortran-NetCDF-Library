c9112100653358fbf5a26c68105de335e493d7bb
han190 <limpid.mazarine@gmail.com>
2023-08-22 13:42:18 -0800

    change test file

--- test/test.f90
+++ test/test.f90
@@ -7,12 +7,17 @@ integer, parameter :: nx = 3, ny = 4
 type(variable_type) :: var
 real, allocatable :: data(:, :)
 
+! Fill data with random numbers.
 allocate (data(nx, ny))
 call random_number(data)
+
+! Construct a data array.
 var = data_array(data, name="data", &
   & dims=dims(["x".dim.nx, "y".dim.ny]), &
-  & atts=atts(["comment".att."dummy variable", "units".att."none"]))
+  & atts=atts(["comment".att."dummy variable"]))
 print "(dt)", var
+
+! Write to netcdf
 ! call to_netcdf(var, "dummy.nc")
 
 end program main
